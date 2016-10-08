dc9b4337031f6cffdc72d0b3332c2d214ca565e2
Jakub <svehl.jakub@gmail.com>
2016-10-08 19:17:59 +0200

    verison 0.0.1 basic logic functions

---
+++ example/main.js
@@ -0,0 +1,43 @@
+import mockData from './mockData';
+import tableToSchema from '../lib/table-to-schema';
+
+
+// configurace
+//kontroluju vstupy na keys??? jestli jsou undefined null?
+const config = [
+  { distinctKey: 'manufact',
+    childrenName: 'manufacts',
+    keys: [ 'manufact', 'manufactId' ]
+  },
+  { distinctKey: 'car',
+    childrenName: 'cars',
+    keys: [ 'car', "carId" ]
+  },
+  { distinctKey: 'engine',
+    childrenName: 'engines',
+    keys: [ 'engine', "engineId" ]
+  },
+  { distinctKey: 'myFunction',
+    keys: [ 'myFunction', "myFunctionId" ]
+  },
+]
+
+document.write(`
+  <table style="padding: 0px 50px; width: 100%">
+    <tr>
+      <td><h2>Table data</h2></td>
+      <td><h2>New schema data</h2></td>
+    </tr>
+    <tr>
+      <td></br>`)
+document.write('<pre>'+JSON.stringify(mockData, null, 2)+'</pre>');
+document.write(`</td><td>`)
+
+
+const results = tableToSchema(config, mockData);
+document.getElementsByTagName("BODY").innerHTML = JSON.stringify(results)
+console.log(results)
+document.write('<pre>'+JSON.stringify(results, null, 2)+'</pre>');
+console.log(tableToSchema)
+
+document.write(`</td><tr></table>`)
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var path = require("path");
 
 module.exports = {
   entry: {
-    app: ["./lib/index.js"]
+    app: ["./example/main.js"]
   },
   output: {
     path: path.resolve(__dirname, "build"),
